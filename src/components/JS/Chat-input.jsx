0412be1adfa8ba54112976c68cd539dcf325deb0
Vasiliy <vasiliy@116.1.168.192.fryazino.net>
2025-12-30 01:23:08 +0300

    /

--- src/components/JS/Chat-input.jsx
+++ src/components/JS/Chat-input.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import "../UX/Chat-input.css"
+import Options from "./Options";
 
 const ChatInput = () => {
+
+    const [openOptions, setOpenOptions] = useState(true);
+
+    const openOptionsFunction = () => {
+        setOpenOptions(!openOptions);
+    }
+
     return (
         <div className="chat-wrapper">
             <h1 className="main-h1">Cognition</h1>
             <h2 className="main-h2">Точность и информативность превыше всего</h2>
             <div className="chat">
                 <textarea placeholder="Задай любой вопрос..." className="chat-input"/>
-                <button className="options-btn">+</button>
-                <button className="send=btn">↑</button>
+                <button className="options-btn" onClick={openOptionsFunction}>+</button>
+                <button className="send-btn">↑</button>
+                {openOptions && <Options />}
             </div>
-            
         </div>
     );
 }
---
+++ src/components/JS/Options.jsx
@@ -0,0 +1,14 @@
+import "../UX/Options.css"
+
+const Options = () => {
+    return (
+        <div className="options-wrapper">
+            <ul className="ul-wrapper">
+                <li className="li">Добавить файл</li>
+                <li className="li">Мультиагентный режим</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Options;
